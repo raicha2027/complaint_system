d9a78d15cd7d3467fa90efab09b967b6d06275cd
Raicha Adamou <raicha.ibrahim@ashesi.edu.gh>
2025-12-18 02:46:31 +0000

    done

--- complaint_system/index.php
+++ complaint_system/index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
-<body>
+<body class="landing-page">
     <nav class="navbar">
         <div class="nav-container">
             <h1 class="logo"><i class="fas fa-graduation-cap"></i> ComplaintSystem</h1>
